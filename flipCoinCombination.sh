a27904db14553a1330fa99d7596ed56129974dc1
Yash-y-ash <wardhan696@gmail.com>
2021-03-05 02:53:48 +0530

    Added:triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,51 +1,89 @@
-#!/bin/bash -x
-totalflip=200
-hh=0
-tt=0
-ht=0
-th=0
+#!/bin/bash
+totalflip=51
+ttt=0
+tth=0
+tht=0
+thh=0
+hhh=0
+hht=0
+hth=0
+htt=0
 for((i=1;i<=$totalflip;i++))
 do
 coin1flip=$((RANDOM%2))
 coin2flip=$((RANDOM%2))
-
-if [ $coin1flip -eq 1 ] && [ $coin2flip -eq 1 ]
+coin3flip=$((RANDOM%2))
+if [ $coin1flip -eq 0 ] && [ $coin2flip -eq 0 ] && [ $coin3flip -eq 0 ]
 then
-let hh++
+let ttt++
 
-elif [ $coin1flip -eq 0 ] && [ $coin2flip -eq 0 ]
+elif [ $coin1flip -eq 0 ] && [ $coin2flip -eq 0 ] && [ $coin3flip -eq 1 ]
 then
-let tt++
+let tth++
 
-elif [ $coin1flip -eq 1 ] && [ $coin2flip -eq 0 ]
+elif [ $coin1flip -eq 0 ] && [ $coin2flip -eq 1 ] && [ $coin3flip -eq 0 ]
 then
-let ht++
+let tht++
 
-elif [ $coin1flip -eq 0 ] && [ $coin2flip -eq 1 ]
+elif [ $coin1flip -eq 0 ] && [ $coin2flip -eq 1 ] && [ $coin3flip -eq 1 ]
 then
-let th++
+let thh++
+
+elif [ $coin1flip -eq 1 ] && [ $coin2flip -eq 1 ] && [ $coin3flip -eq 1 ]
+then
+let hhh++
+
+elif [ $coin1flip -eq 1 ] && [ $coin2flip -eq 1 ] && [ $coin3flip -eq 0 ]
+then
+let hht++
+
+elif [ $coin1flip -eq 1 ] && [ $coin2flip -eq 0 ] && [ $coin3flip -eq 1 ]
+then
+let hth++
+
+elif [ $coin1flip -eq 1 ] && [ $coin2flip -eq 0 ] && [ $coin3flip -eq 0 ]
+then
+let htt++
+
 
 fi
 done
 
 declare -A dr
 
-dr[hh]=$hh
-dr[tt]=$tt
-dr[ht]=$ht
-dr[th]=$th
+dr[ttt]=$ttt
+dr[tth]=$tth
+dr[tht]=$tht
+dr[thh]=$thh
+dr[hhh]=$hhh
+dr[hht]=$hht
+dr[hth]=$hth
+dr[htt]=$htt
+
+
 
 echo ${dr[@]}
 
-prhh=`echo $hh $totalflip | awk '{a=$1/$2*100} {print a}'`
-prtt=`echo $tt $totalflip | awk '{a=$1/$2*100} {print a}'`
-prht=`echo $ht $totalflip | awk '{a=$1/$2*100} {print a}'`
-prth=`echo $th $totalflip | awk '{a=$1/$2*100} {print a}'`
+prttt=`echo $ttt $totalflip | awk '{a=$1/$2*100} {print a}'`
+prtth=`echo $tth $totalflip | awk '{a=$1/$2*100} {print a}'`
+prtht=`echo $tht $totalflip | awk '{a=$1/$2*100} {print a}'`
+prthh=`echo $thh $totalflip | awk '{a=$1/$2*100} {print a}'`
 
-echo "percentage of hh combination is $prhh"
-echo "percentage of tt combination is $prtt"
-echo "percentage of ht combination is $prht"
-echo "percentage of th combination is $prth"
+prhhh=`echo $hhh $totalflip | awk '{a=$1/$2*100} {print a}'`
+prhht=`echo $hht $totalflip | awk '{a=$1/$2*100} {print a}'`
+prhth=`echo $hth $totalflip | awk '{a=$1/$2*100} {print a}'`
+prhtt=`echo $htt $totalflip | awk '{a=$1/$2*100} {print a}'`
+
+
+echo "percentage of ttt combination is $prttt"
+echo "percentage of tth combination is $prtth"
+echo "percentage of tht combination is $prtht"
+echo "percentage of thh combination is $prthh"
+
+echo "percentage of hhh combination is $prhhh"
+echo "percentage of hht combination is $prhht"
+echo "percentage of hth combination is $prhth"
+echo "percentage of htt combination is $prhtt"
 
 
 
