7a77e8fb7b2b434701a6eb065ad6693f49a181fb
Yash-y-ash <wardhan696@gmail.com>
2021-03-05 01:19:21 +0530

    Added:singlet combition and their percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,7 +1,26 @@
 #!/bin/bash -x
+hcount=0
+tcount=0
+totalflip=200
+for((i=1;i<=$totalflip;i++))
+do
 flip=$((RANDOM%2))
 if [ $flip -eq 1 ];then
-echo "Head"
+let hcount++
 else
-echo "Tail"
+let tcount++
 fi
+done
+
+declare -A dr
+
+dr[head]=$hcount
+dr[tail]=$tcount
+
+echo "${dr[@]}"
+
+headpr=`echo $hcount $totalflip | awk '{a=$1/$2*100} {print a}'`
+echo "head percentage $headpr %"
+
+tailpr=`echo $tcount $totalflip | awk '{b=$1/$2*100} {print b}'`
+echo "tail percentage $tailpr %"
